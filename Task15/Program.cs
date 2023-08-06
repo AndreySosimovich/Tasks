80e166ecda9ad85c4c3cf07ea670b978997c114d
«Andrey» <sandreyv@bk.ru>
2023-08-06 13:07:52 +0300

    edit Tasks15

---
+++ Task15/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите цифру, которая соответствует дню недели");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool result = WeekDigit(number, 6, 7);
+Console.WriteLine(result? "Да, этот день выходной": "Нет, это не выходной день");
+
+bool WeekDigit(int num, int num1, int num2)
+{
+   return (num == num1 || num == num2);
+}
