e6d7ed48e03f2e4df27b808a95c06ffcdf307e3b
«Andrey» <sandreyv@bk.ru>
2023-09-14 17:09:03 +0300

    Added Tasks 64, 66, 68

---
+++ Task66/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму 
+// натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Задайте первое натуральное число: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте второе натуральное число: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+if (numberM < 1 || numberN < 1)
+{
+    Console.WriteLine("Ошибка вввода! ");
+    return;
+}
+
+Console.Write($"M = {numberM}; N = {numberN} -> ");
+
+if (numberM > numberN)
+{
+    int temp = numberM;
+    numberM = numberN;
+    numberN = temp;
+}
+
+int SumNaturalDigit(int numM, int numN)
+{
+    if (numM == numN) return 0;
+    else
+    {
+        numM++;
+        return numM + SumNaturalDigit(numM, numN);
+    }
+}
+Console.Write(SumNaturalDigit(numberM - 1, numberN));
+
+ 
---
+++ Task68/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+//Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.WriteLine("Задайте первое неотрицательное число: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте второе неотрицательное число: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+if (numberM < 0 || numberN < 0)
+{
+    Console.WriteLine("Ошибка вввода! ");
+    return;
+}
+
+Console.Write($"m = {numberM}; n = {numberN} -> A (m,n) = ");
+Console.WriteLine(FunctionAkkerman(numberM, numberN));
+
+int FunctionAkkerman(int numM, int numN)
+{
+    if (numM == 0)
+    {
+        return numN + 1;
+    }
+    else if (numN == 0)
+    {
+        return FunctionAkkerman(numM - 1, 1);
+    }
+    else
+    {
+        return (FunctionAkkerman(numM - 1, FunctionAkkerman(numM, numN - 1)));
+    }
+}
